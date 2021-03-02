9123d75c50f2681c3a7f4080bbeda352532655a9
ljsoo123 <ljsoo123456@naver.com>
2021-03-02 14:39:34 +0900

    [MODIFY] html 웹팩

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/test.js", //웹팩 빌드할 파일
@@ -8,4 +9,23 @@ module.exports = {
     path: path.resolve(__dirname + "/build"),
   },
   mode: "none", //웹팩 옵션
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", //이 파일을 읽는다
+      filename: "index.html", //output으로 출력할 파일
+    }),
+  ],
 };
